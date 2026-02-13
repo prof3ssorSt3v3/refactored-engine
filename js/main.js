8d328c4200d3690db9596f2c70685a9fccda9a39
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2026-02-13 12:40:37 -0500

    end of class

--- js/main.js
+++ js/main.js
@@ -1,14 +1,34 @@
 document.addEventListener('DOMContentLoaded', init);
 const log = console.log;
 const warn = console.warn;
+const APP = {
+  isStandalone: false,
+};
 
 function init() {
   //dom ready
   registerSW();
+  // document.getElementById('submitButton').click();
+  // document.querySelector('video').play();
+  document.querySelector('footer>a').addEventListener('click', (ev) => {
+    ev.preventDefault();
+    console.log('Thanks for clicking');
+  });
+
+  if (navigator.standalone) {
+    console.log('Launched: Installed (iOS)');
+    APP.isStandalone = true;
+  } else if (matchMedia('(display-mode: standalone)').matches) {
+    console.log('Launched: Installed');
+    APP.isStandalone = true;
+  } else {
+    console.log('Launched: Browser Tab');
+    APP.isStandalone = false;
+  }
 }
 
 function registerSW() {
-  navigator.serviceWorker.register('/sw.js', { type: 'module' }).then((reg) => {
+  navigator.serviceWorker.register('./sw.js', { type: 'module' }).then((reg) => {
     console.log('service worker is registered');
   });
 }
--- sw.js
+++ sw.js
@@ -1,19 +1,51 @@
-const version = 1;
+const version = 6;
 const cacheName = `HelloMonday-${version}`;
 
-const cacheList = ['/', '/index.html', '/css/main.css', '/js/main.js'];
+const cacheList = [
+  '/',
+  '/index.html',
+  '/css/main.css',
+  '/js/main.js',
+  '/manifest.json',
+  '/img/apple-touch-icon.png',
+  '/img/favicon-96x96.png',
+  '/img/favicon.ico',
+  '/img/favicon.svg',
+  '/img/web-app-manifest-192x192.png',
+  '/img/web-app-manifest-512x512.png',
+  'https://fonts.googleapis.com/css2?family=Noto+Sans+Mono:wght@100..900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Noto+Serif:ital,wght@0,100..900;1,100..900&display=swap',
+];
 
 self.addEventListener('install', (ev) => {
   //add all the files from cache list
+  //trigger a new download and saving to the cache of these cacheList files
+  caches.open(cacheName).then((cache) => {
+    cache.addAll(cacheList).catch((err) => {
+      console.log(err, 'while saving all the cacheList');
+    });
+  });
 });
 
 self.addEventListener('activate', (ev) => {
   //remove old cache version files
+  caches.keys().then((keys) => {
+    //keys needs to be the array of things we WANT to delete
+    keys
+      .filter((key) => key != cacheName)
+      .forEach((key) => {
+        //for each cache name that is still in the array... delete
+        caches.delete(key);
+      });
+  });
 });
 
-self.addEventListener('fetch', (ev) => {
-  //handle different file requests based on properties and online status
-});
+// self.addEventListener('fetch', (ev) => {
+//   //handle different file requests based on properties and online status
+//   //This fetch listener is the LAST thing that you want to build in your project
+//   // ev.respondWith(new Response(null, { status: 404 }));
+//   ev.respondWith(fetch(ev.request));
+//   //this is just a pass-thru to ask for the file that the browser asked for
+// });
 
 self.addEventListener('message', (ev) => {
   //receive messages from the JS files from the website
